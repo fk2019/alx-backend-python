8680063d4bef670d21260ad8b0db2e2249cbe6b9
fk2019 <fkmuiruri8@gmail.com>
2023-07-31 10:26:13 +0300

    Add to_str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Return string represenation of float
+"""
+
+
+def to_str(n: float) -> str:
+    """String representation of float"""
+    return str(n)
---
+++ 0x00-python_variable_annotations/3-to_str.py~
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Return floor of float
+"""
+
+
+def floor(n: float) -> int:
+    """Floor a float"""
+    return int(n)
