19f324d30886e1464305463c5615ca61ba9b09d1
fk2019 <fkmuiruri8@gmail.com>
2023-08-02 10:05:43 +0300

    Add sum-list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""sum_list returns sum of list and return as flaot
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Return sum of list items"""
+    return sum(input_list)
