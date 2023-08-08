34f2e61cc2debe48ce4d56f93c8448f697bcf405
fk2019 <fk2019@gmail.com>
2023-08-08 20:33:52 +0300

    Annotate function

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """async_generator: loop 10 times yielding 0-10
 """
+from typing import Generator
 import asyncio
 import random
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """Yield 0-10"""
     for _ in range(10):
         await asyncio.sleep(1)
