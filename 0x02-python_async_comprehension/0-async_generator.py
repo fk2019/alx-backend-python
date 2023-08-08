a16cd57493d4a6a9f426d33de9ddd84ecea96206
fk2019 <fk2019@gmail.com>
2023-08-08 20:31:16 +0300

    Add async_generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""async_generator: loop 10 times yielding 0-10
+"""
+import asyncio
+import random
+
+
+async def async_generator():
+    """Yield 0-10"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield (random.random() * 10)
