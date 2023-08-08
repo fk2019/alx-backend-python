20bd1fbf7304683702d4b07ff6987d806d3ce37b
fk2019 <fk2019@gmail.com>
2023-08-08 20:52:35 +0300

    Annotate function

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
-"""async_comprehension: collect and return 10 random numbers using async
- comprehension
+"""async_comprehension: collect and return a list of 10 random
+   numbers using async
+   comprehension
 """
-from typing import Generator
+from typing import List
 import asyncio
 import random
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
+async def async_comprehension() -> List[float]:
     """return 10 random numbers"""
     result = [i async for i in async_generator()]
     return result
