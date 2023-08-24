2359d73af8bb973be0cdcc42295cde06a364b7ec
fk2019 <fkmuiruri8@gmail.com>
2023-08-24 12:46:44 +0300

    Add test utils

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,25 +2,18 @@
 """A module for parametarizing a unit test
 """
 import unittest
-from unittest.mock import Mock, patch
 from parameterized import parameterized
 from typing import (
     Dict,
     Mapping,
     Sequence,
-    Tuple,
     Union,
 )
-from utils import (
-    access_nested_map,
-    get_json,
-    memoize,
-)
+from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
     """Test case for access_nested_map"""
-
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
@@ -30,65 +23,7 @@ class TestAccessNestedMap(unittest.TestCase):
             self,
             nested_map: Mapping,
             path: Sequence,
-            expected: Union[Dict, int]
+            expected_result: Union[Dict, int]
             ) -> None:
-        """Tests `access_nexted_map`'s output"""
-        self.assertEqual(access_nested_map(nested_map, path), expected)
-
-    @parameterized.expand([
-        ({}, ("a",), KeyError),
-        ({"a": 1}, ("a", "b"), KeyError),
-    ])
-    def test_access_nested_map_exception(
-            self,
-            nested_map: Mapping,
-            path: Sequence,
-            exception: Exception,
-            ) -> None:
-        """Tests `access_nested_map`'s exception raising.
-        """
-        with self.assertRaises(exception):
-            access_nested_map(nested_map, path)
-
-
-class TestGetJson(unittest.TestCase):
-    """Tests the get_json method"""
-
-    @parameterized.expand([
-        ("http://example.com", {"payload": True}),
-        ("http://holberton.io", {"payload": False}),
-    ])
-    def test_get_json(
-            self,
-            test_url: str,
-            test_payload: Dict,
-            ) -> None:
-        """Tests get_json's output"""
-
-        attrs = {'json.return_value': test_payload}
-
-        with patch("requests.get", return_value=Mock(**attrs)) as req_get:
-            self.assertEqual(get_json(test_url), test_payload)
-            req_get.assert_called_once_with(test_url)
-
-
-class TestMemoize(unittest.TestCase):
-    """Tests the memoize method"""
-    def test_memoize(self) -> None:
-        """Tests the output."""
-        class TestClass:
-            def a_method(self):
-                return 54
-
-            @memoize
-            def a_property(self):
-                return self.a_method()
-        with patch.object(
-                TestClass,
-                "a_method",
-                return_value=lambda: 54,
-                ) as memo_func:
-            test_class = TestClass()
-            self.assertEqual(test_class.a_property(), 54)
-            self.assertEqual(test_class.a_property(), 54)
-            memo_func.assert_called_once()
+        """Tests access_nested_map's output"""
+        self.assertEqual(access_nested_map(nested_map, path), expected_result)
